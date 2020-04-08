9849a1ca2ec81c695ebd6bfab6c6f84bdf22d30c
Tyler Pawlaczyk <pawlactb@gmail.com>
2020-04-08 12:06:52 -0400

    laid out structure of PortmantoutNode.

---
+++ cs451/PortmantoutNode.py
@@ -0,0 +1,68 @@
+from cs451.Node import Node
+
+
+class PortmantoutNode(Node):
+    syllables = {}
+
+    @staticmethod
+    def get_syllables(word):
+        """Query the generated list of syllables for a word's syllables.
+
+        :param word: word to return syllables of
+        :type word: str
+        :return: List of syllables
+        :rtype: [str]
+        """
+        return PortmantoutNode.syllables[word]
+
+    def __init__(self, syllables, *args, **kwargs):
+        if syllables:
+            PortmantoutNode.syllables = syllables
+        Node.__init__(self, args, kwargs)
+
+    # TODO:
+    def is_valid(self):
+        """Check if a valid portmantaeu is made from the words in self.path.
+        (Might only need to check most recent word added? Since we don't generate successors in invalid portmanteaus.)
+
+        :return: True iff self.path can be a portmanteau, False otherwise.
+        :rtype: Boolean
+        """
+        return False
+
+    # TODO:
+    def is_complete(self):
+        """Check if node is at a terminal depth in search.
+
+        :return: True iff node is at terminal depth.
+        :rtype: Boolean
+        """
+        return False
+
+    # TODO:
+    def goal_test(self):
+        """Check if the node met the goal requirements.
+
+        :return: True iff node is at terminal depth, and node meets goal requirements.
+        :rtype: Boolean
+        """
+        if not self.is_complete():
+            return False
+        return False
+
+    # TODO:
+    def successors(self):
+        """This function should return a list of PortmantoutNode s each with one of the remaining words appended to the successor's path.
+
+            :return: If not self.valid(), returns [], else, returns a List of PortmantoutNodes (that may not be valid).
+            :rtype: List
+        """
+        return []
+
+    def cost(self):
+        """Return the number of words added to the PortmantoutNode.
+
+        :return: Number of words on path
+        :rtype: int
+        """
+        return len(self.path)
